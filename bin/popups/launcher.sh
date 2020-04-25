4a019a260085df3f097fd70479bb080c0e656be2
Samuel Hand <samuel.d.hand@gmail.com>
2020-04-26 00:57:42 +0100

    Fix scripts

--- bin/popups/launcher.sh
+++ bin/popups/launcher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-rofi -kb-cancel "Escape" -modi drun -show drun -theme $HOME/.config/rofi/launchers/style_icons_popup.rasi &
+rofi -kb-cancel "Escape" -modi drun -show drun &
 launcher=$!
 sleep .13; n30f ~/.n30f/arrow-blue.png -i -x 45 -y 974 &
 n30f=$!
--- bin/popups/switcher.sh
+++ bin/popups/switcher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-rofi -kb-cancel "Escape" -modi windowcd -show windowcd -theme $HOME/.config/rofi/launchers/style_icons_popup.rasi &
+rofi -kb-cancel "Escape" -modi windowcd -show windowcd &
 launcher=$!
 sleep .13; n30f ~/.n30f/arrow-blue.png -i -x 45 -y 974 &
 n30f=$!
